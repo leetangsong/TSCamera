202ddf4167ba78c9cecc0eee8350094cba62a5b8
litangsong <leetangsong@icloud.com>
2022-07-04 17:57:55 +0800

    no message

--- Example/TSCamera/TSGridView.swift
+++ Example/TSCamera/TSGridView.swift
@@ -52,56 +52,130 @@ class TSGridMaskLayer: CAShapeLayer{
 class TSGridLayer: CAShapeLayer{
     ///网格区域
     var gridRect: CGRect = .zero
-    ///网格颜色  默认黑色
-    var gridColor: UIColor = .black
+    ///网格颜色  默认白色
+    var gridColor: UIColor = .white
     ///背景  默认透明
     var bgColor: UIColor = .clear
     
+    ///边框线
+    lazy var borderLayer: CAShapeLayer = CAShapeLayer()
+    ///网格线
+    lazy var gridLayer: CAShapeLayer = CAShapeLayer()
+    ///边角线
+    lazy var cornerLayer: CAShapeLayer = CAShapeLayer()
+    
     override init() {
         super.init()
-        contentsScale = UIScreen.main.scale
-        shadowColor = UIColor.black.cgColor
-        shadowRadius = 3
-        shadowOffset = .zero
-        shadowOpacity = 0.5
+        
+        for layer in [borderLayer, gridLayer, cornerLayer] {
+            layer.contentsScale = UIScreen.main.scale
+            layer.shadowColor = UIColor.black.cgColor
+            layer.shadowRadius = 3
+            layer.shadowOffset = .zero
+            layer.shadowOpacity = 0.5
+            addSublayer(layer)
+        }
+        
     }
     
     
     func setGridRect(_ gridRect: CGRect, animated: Bool){
         if !gridRect.equalTo(self.gridRect) {
             self.gridRect = gridRect
-            let path = drawGrid()
+            
+            let borderPath = drawBorder()
+            let gridPath = drawGrid()
+            let cornerPath = drawCorner()
+            
             if animated {
-                let animation = CABasicAnimation.init(keyPath: "path")
-                animation.duration = 1
-                animation.fromValue = self.path
-                animation.toValue = path
-                add(animation, forKey: "gridLayer_contentsRectAnimation")
+                addAnimation(with: borderLayer, path: borderPath)
+                addAnimation(with: gridLayer, path: gridPath)
+                addAnimation(with: cornerLayer, path: cornerPath)
+            }else{
+                borderLayer.path = borderPath
+                gridLayer.path = gridPath
+                cornerLayer.path = cornerPath
             }
-            self.path = path
+           
         }
     }
     
-    func drawGrid() -> CGPath{
-        fillColor = bgColor.cgColor
-        strokeColor = gridColor.cgColor
-        
-        
-        
-        var dWidth = 0
-        for i in 0...3 {
-            
-        }
+    func addAnimation(with layer: CAShapeLayer, path: CGPath){
+        layer.removeAnimation(forKey: "gridLayer_contentsRectAnimation")
+        let animation = CABasicAnimation.init(keyPath: "path")
+        animation.duration = 0.25
+        animation.fromValue = layer.path
+        animation.toValue = path
+        animation.isRemovedOnCompletion = false
+        layer.path = path
+        layer.add(animation, forKey: "gridLayer_contentsRectAnimation")
+    }
+    func drawBorder() -> CGPath{
+        borderLayer.lineWidth = 1
+        borderLayer.fillColor = bgColor.cgColor
+        borderLayer.strokeColor = gridColor.cgColor
         let path = UIBezierPath()
         ///边框
         path.move(to: CGPoint.init(x: gridRect.origin.x, y: gridRect.origin.y))
         path.addLine(to: CGPoint.init(x: gridRect.maxX, y: gridRect.origin.y))
         path.addLine(to: CGPoint.init(x: gridRect.maxX, y: gridRect.maxY))
+        path.addLine(to: CGPoint.init(x: gridRect.origin.x, y: gridRect.maxY))
+        path.addLine(to: CGPoint.init(x: gridRect.origin.x, y: gridRect.origin.y))
+        return path.cgPath
+    }
+    func drawGrid() -> CGPath{
+        gridLayer.lineWidth = 0.3
+        gridLayer.fillColor = bgColor.cgColor
+        gridLayer.strokeColor = gridColor.cgColor
+        let path = UIBezierPath()
+        ///横线
+        for i in 1...2 {
+            path.move(to: CGPoint.init(x: gridRect.minX, y: gridRect.minY + CGFloat(i)*gridRect.height/3))
+            path.addLine(to: CGPoint.init(x: gridRect.minX+gridRect.width, y: gridRect.minY + CGFloat(i)*gridRect.height/3))
+        }
         
+        ///竖线
+        for i in 1...2 {
+            path.move(to: CGPoint.init(x: gridRect.minX + CGFloat(i)*gridRect.width/3, y: gridRect.minY))
+            path.addLine(to: CGPoint.init(x: gridRect.minX + CGFloat(i)*gridRect.width/3, y: gridRect.minY+gridRect.height))
+        }
+        return path.cgPath
+    }
+    func drawCorner() -> CGPath{
+        cornerLayer.lineWidth = 3
+        cornerLayer.fillColor = bgColor.cgColor
+        cornerLayer.strokeColor = gridColor.cgColor
+        
+        let path = UIBezierPath()
+        let newRect = gridRect.insetBy(dx: -1, dy: -1)
+        let cornerlength = 15.0
+        ///左上角
+        path.move(to: CGPoint.init(x: newRect.minX, y: newRect.minY+cornerlength))
+        path.addLine(to: CGPoint.init(x: newRect.minX, y: newRect.minY))
+        path.addLine(to: CGPoint.init(x: newRect.minX+cornerlength, y: newRect.minY))
+        
+        
+        ///右上角
+        path.move(to: CGPoint.init(x: newRect.maxX-cornerlength, y: newRect.minY))
+        path.addLine(to: CGPoint.init(x: newRect.maxX, y: newRect.minY))
+        path.addLine(to: CGPoint.init(x: newRect.maxX, y: newRect.minY+cornerlength))
+        
+        ///左下角
+        path.move(to: CGPoint.init(x: newRect.minX, y: newRect.maxY-cornerlength))
+        path.addLine(to: CGPoint.init(x: newRect.minX, y: newRect.maxY))
+        path.addLine(to: CGPoint.init(x: newRect.minX+cornerlength, y: newRect.maxY))
+        
+        ///右下角
+        path.move(to: CGPoint.init(x: newRect.maxX-cornerlength, y: newRect.maxY))
+        path.addLine(to: CGPoint.init(x: newRect.maxX, y: newRect.maxY))
+        path.addLine(to: CGPoint.init(x: newRect.maxX, y: newRect.maxY-cornerlength))
         
         return path.cgPath
     }
     
+    override func layoutSublayers() {
+        super.layoutSublayers()
+    }
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
--- Example/TSCamera/ViewController.swift
+++ Example/TSCamera/ViewController.swift
@@ -18,9 +18,22 @@ class ViewController: UIViewController {
         button.frame = CGRect.init(x: 100, y: 100, width: 100, height: 100)
         view.addSubview(button)
         button.addTarget(self, action: #selector(click), for: .touchUpInside)
+       
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        let grid = TSGridLayer()
+        grid.frame = view.bounds
+        grid.setGridRect(CGRect.init(x: 100, y: 250, width: 200, height: 200), animated: true)
+        view.layer.addSublayer(grid)
+        view.backgroundColor = .black
+        
+        DispatchQueue.main.asyncAfter(deadline: .now()+2) {
+            grid.setGridRect(CGRect.init(x: 100, y: 300, width: 200, height: 300), animated: true)
+        }
+    }
     @objc func click(){
         self.present(TSCameraViewController(), animated: true, completion: nil)
     }
